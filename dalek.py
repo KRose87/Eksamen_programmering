7bb6e3123aa3ffc56f39d422866821d7602897a0
KRose87 <107762942+KRose87@users.noreply.github.com>
2022-10-24 21:59:18 +0200

    Update dalek.py

--- dalek.py
+++ dalek.py
@@ -10,9 +10,6 @@ class Dalek:
         self.dalek_img = pygame.image.load("img/dalek.png")
         self.dalek_rect = self.dalek_img.get_rect(center=(self.xpos, self.ypos))
 
-    def killme(self):
-        self.dalek_rect.centerx = self.settings.screen_width + 800
-
     def blitme(self):
         self.screen.blit(self.dalek_img, self.dalek_rect)
 
